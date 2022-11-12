524614c18ef17bace5ba36d900ee722ee8437b1e
Marker97 <chereche10@gmail.com>
2022-11-12 18:02:48 +0100

    Input and Output_03

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String name = "Marcus";
+        String surname = "Marziano";
+        System.out.println(name + "\n" + surname);
+        System.out.printf("Hi! My name is %s. \nMy surname is %s.", name, surname);
+    }
+}
